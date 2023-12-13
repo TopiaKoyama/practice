9703b77ae60169a3360b9c1b9875aa71f035a539
koyama <37497795+TopiaKoyama@users.noreply.github.com>
2023-12-13 09:38:26 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,29 @@
+public abstract class TangibleAsset extends Asset implements Thing{
+	String color;
+	double weight;
+	public TangibleAsset(String name, int price, String color) {
+		super(name, price);
+		this.color = color;
+	}
+
+	public TangibleAsset(String name, int price, String color, double weight) {
+		super(name, price);
+		this.color = color;
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	@Override
+	public double getWeight() {
+		return weight;
+	}
+
+	@Override
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+}
